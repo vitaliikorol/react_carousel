0d741b922e75aa4b55bd42c6cbf8d53502da592d
Vitalii Korol <vitaliikorol@192-168-0-105.rdsnet.ro>
2020-05-09 18:45:43 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c3eae33d7ac1f65680fe9ea5b458330e.js"
+  "./precache-manifest.59054f563b4318d2422333e1ade89219.js"
 );
 
 self.addEventListener('message', (event) => {
